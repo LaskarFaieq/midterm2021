c60b085cde6bb8cf299be3408cd79507c5a74129
LaskarFaieq <laskarfaieq@gmail.com>
2021-10-17 17:24:23 -0400

    fibonacci and prime numbers

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -1,13 +1,15 @@
 package math;
 
-import java.util.Arrays;
-
 public class Fibonacci {
     public static void main(String[] args) {
-         /*
-          Write 40 Fibonacci numbers with java.
-         */
+        int prev = 0;
+        int next = 1;
 
+        for (int i = 0; i <15; i++) {
+            System.out.println(prev);
+            prev = next - prev;
+            next = next + prev;
 
+         }
     }
 }
--- src/math/PrimeNumber.java
+++ src/math/PrimeNumber.java
@@ -3,6 +3,25 @@ package math;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
+			int i = 0;
+			int num = 0;
+			String primeNumbers = " ";
+
+			for (i = 1; i < 1000000; i++) {
+				int counter = 0;
+				for (num = i; num >= 1; num--) {
+					if(i % num == 0) {
+						counter = counter + 1;
+					}
+				}
+				if (counter == 2) {
+					primeNumbers = primeNumbers + i + " ";
+				}
+			}
+			System.out.println("prime numbers are: ");
+			System.out.println(primeNumbers);
+		}
+	}
 		/*
 		 * Find list of Prime numbers from number 2 to 1 million.
 		 * Try the best solution as possible.Which will take less CPU life cycle.
@@ -16,6 +35,6 @@ public class PrimeNumber {
 		//implementation here...
 
 
-	}
 
-}
+
+
